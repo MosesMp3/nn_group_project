19d541020349b5f861a1a165e35a0c1ebc168416
rpham0306 <ryanlampham1@gmail.com>
2026-04-04 15:49:57 -0500

    added remapping of int labels, and normalized pixel numbers from 0-255 to 0-1

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
-
 # layers we need, we can add more later if needed
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
@@ -10,6 +9,9 @@ from tensorflow.keras.layers import BatchNormalization, Dropout
 from tensorflow.keras.layers import Flatten, Dense
 
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# remap labels to 0-14
+from sklearn.preprocessing import LabelEncoder
 
 import numpy as np
 import random
@@ -40,6 +42,11 @@ with open(val_path, "rb") as f:
 print("Train keys:", train_data.keys())
 print("Val keys:", val_data.keys())
 
+# remap labels from 0-14 pt. 2 > currently random ints such as 163, 28, 6, 189, etc.
+le = LabelEncoder()
+y_train = le.fit_transform(y_train)
+y_val = le.transform(y_val)
+
 
 X_train = train_data["images"]
 y_train = train_data["labels"]
@@ -52,6 +59,14 @@ X_train = np.array(X_train)
 y_train = np.array(y_train)
 X_val = np.array(X_val)
 y_val = np.array(y_val)
+
+# rescaling data from large numbers to numberss from 0 to 1
+X_train = X_train.astype("float32") / 255.0
+X_val   = X_val.astype("float32")   / 255.0
+
+print(f"Image shape: {X_train[0].shape}")
+print(f"Pixel range: {X_train.min()} - {X_train.max()}")
+print(f"Label example: {y_train[0]}")
 
 print(f"Training data shape: {X_train.shape}")
 print(f"Training labels shape: {y_train.shape}")
